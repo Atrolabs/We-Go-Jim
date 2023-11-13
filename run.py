1957fcbad1fb9d24cc7cff1c1d700863ffa07c32
xd4niel <danyxd01@gmail.com>
2023-11-13 21:18:57 +0100

    Added routes to view the login and registration pages

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
-from app.routes import home_bp
+from app.routes import home_bp, login_bp, register_bp
 
 app = Flask(__name__, template_folder='app/templates')
 
 # Register the blueprint
 app.register_blueprint(home_bp)
+app.register_blueprint(login_bp)
+app.register_blueprint(register_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
