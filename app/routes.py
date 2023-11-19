eee8103c536f26858bcde0861d3d3a9ad817bdf5
juhum <anddd144@gmail.com>
2023-11-19 13:57:51 +0100

    Include Checkbox for Student as Default and Output Form Information in JSON to Console

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 
 home_bp = Blueprint("home", __name__)
 login_bp = Blueprint("login", __name__)
 register_bp = Blueprint("register", __name__)
+submit_bp = Blueprint("submit", __name__)
 
 @home_bp.route('/', methods=['GET'])
 def home():
@@ -15,3 +16,17 @@ def login():
 @register_bp.route('/register', methods=['GET', 'POST'])
 def register():
     return render_template("register.html")
+
+@submit_bp.route('/submit', methods=['POST'])
+def submit():
+    data = {}
+
+    # Get the JSON data from the request
+    json_data = request.json
+
+    data['role'] = 'Trainer' if json_data.get('isTrainer') else 'Student'
+
+    #print(data)
+    print(json_data)
+
+    return 'Form submitted successfully!'
--- app/static/js/register.js
+++ app/static/js/register.js
@@ -1,1 +1,42 @@
-// here include js file
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        let userRole;
+        if (document.getElementById('isTrainer').checked) {
+            userRole = 'Trainer';
+        } else if (document.getElementById('isStudent').checked) {
+            userRole = 'Student';
+        }
+
+        // Create a form data object from the form element
+        const formData = new FormData(form);
+
+        // Convert the form data object to a JSON object
+        const jsonData = Object.fromEntries(formData.entries());
+
+        // Send JSON data to the server
+        fetch('/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(jsonData),
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
+    });
+});
+
+
+function toggleCheckboxes(checkboxId) {
+    const checkboxes = ['isTrainer', 'isStudent'];
+    checkboxes.forEach(id => {
+        if (id !== checkboxId) {
+            document.getElementById(id).checked = false;
+        }
+    });
+}
