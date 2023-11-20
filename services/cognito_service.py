5c054b1c0041a4738394b72810a6bc1abff31406
Atrolide <loszewskigrzegorz@gmail.com>
2023-11-20 15:47:53 +0100

    Provided user login feature

--- services/cognito_service.py
+++ services/cognito_service.py
@@ -5,38 +5,61 @@ from utils.cognito_utils import calculate_secret_hash
 
 class CognitoService():
     """
-    A class representing a service for interacting with Amazon Cognito for user registration.
+    A class representing a service for interacting with Amazon Cognito for user registration and login.
 
-    Attributes:
-        - user_pool_id (str): The ID of the Cognito user pool.
-        - app_client_id (str): The app client ID associated with the Cognito user pool.
-        - app_client_secret (str): The app client secret associated with the Cognito app client.
-        - client (boto3.client): An instance of the Boto3 Cognito Identity Provider client.
+Attributes:
+    - user_pool_id (str): The ID of the Cognito user pool.
+    - app_client_id (str): The app client ID associated with the Cognito user pool.
+    - app_client_secret (str): The app client secret associated with the Cognito app client.
+    - client (boto3.client): An instance of the Boto3 Cognito Identity Provider client.
 
-    Methods:
-        - register_user(email: str, password: str, user_type: str) -> dict:
-            Registers a new user in the Cognito user pool.
+Methods:
+    - register_user(email: str, password: str, user_type: str) -> dict:
+        Registers a new user in the Cognito user pool.
 
-            Parameters:
-                - email (str): The email address of the user.
-                - password (str): The password for the user.
-                - user_type (str): The type of the user.
+        Parameters:
+            - email (str): The email address of the user.
+            - password (str): The password for the user.
+            - user_type (str): The type of the user.
 
-            Returns:
-                - dict: A dictionary containing the response from the Cognito service.
+        Returns:
+            - dict: A dictionary containing the response from the Cognito service.
 
-            Raises:
-                - Exception: If an error occurs during user registration.
+        Raises:
+            - Exception: If an error occurs during user registration.
 
-            Example:
-                ```python
-                cognito_service = CognitoService()
-                response = cognito_service.register_user(email="user@example.com", password="password123", user_type="standard")
-                print(response)
-                ```
+        Example:
+            ```python
+            cognito_service = CognitoService()
+            response = cognito_service.register_user(email="user@example.com", password="password123", user_type="standard")
+            print(response)
+            ```
 
-            Note:
-                Make sure to handle exceptions appropriately when calling this method.
+        Note:
+            Make sure to handle exceptions appropriately when calling this method.
+
+    - login_user(email: str, password: str) -> dict:
+        Logs in a user with the provided credentials in the Cognito user pool.
+
+        Parameters:
+            - email (str): The email address of the user.
+            - password (str): The password for the user.
+
+        Returns:
+            - dict: A dictionary containing the response from the Cognito service.
+
+        Raises:
+            - Exception: If an error occurs during user login.
+
+        Example:
+            ```python
+            cognito_service = CognitoService()
+            response = cognito_service.login_user(email="user@example.com", password="password123")
+            print(response)
+            ```
+
+        Note:
+            Make sure to handle exceptions appropriately when calling this method.
     """
 
     def __init__(self):
@@ -88,3 +111,44 @@ class CognitoService():
             return response
         except ClientError as e:
             raise Exception(f"Error registering user: {e}")
+
+    def login_user(self, email: str, password: str) -> dict:
+        """
+        Logs in a user with the provided credentials in the Cognito user pool.
+
+        Parameters:
+            - email (str): The email address of the user.
+            - password (str): The password for the user.
+
+        Returns:
+            - dict: A dictionary containing the response from the Cognito service.
+
+        Raises:
+            - Exception: If an error occurs during user login.
+
+        Example:
+            ```python
+            cognito_service = CognitoService()
+            response = cognito_service.login_user(email="user@example.com", password="password123")
+            print(response)
+            ```
+
+        Note:
+            Make sure to handle exceptions appropriately when calling this method.
+        """
+        try:
+            secret_hash = calculate_secret_hash(APP_CLIENT_SECRET, APP_CLIENT_ID, email)
+
+            response = self.client.initiate_auth(
+                AuthFlow='USER_PASSWORD_AUTH',
+                AuthParameters={
+                    'USERNAME': email,
+                    'PASSWORD': password,
+                    'SECRET_HASH': secret_hash,
+                },
+                ClientId=self.app_client_id,
+            )
+
+            return response
+        except ClientError as e:
+            raise Exception(f"Error logging in user: {e}")
