b505216d57d7fbde161d834f9b95964d01971d5b
Atrolide <loszewskigrzegorz@gmail.com>
2023-11-20 12:10:29 +0100

    Missing Docstrings for routes.py Hotfix

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, render_template, request, jsonify
-from services.cognito_service import CognitoService  # Import the CognitoService
+from services.cognito_service import CognitoService 
+from typing import Union, Dict
 
+# Define Flask Blueprints for different parts of the application
 home_bp = Blueprint("home", __name__)
 login_bp = Blueprint("login", __name__)
 register_bp = Blueprint("register", __name__)
 
-cognito_service = CognitoService()  # Create an instance of CognitoService
+# Create an instance of CognitoService to interact with Amazon Cognito
+cognito_service = CognitoService()  
 
 @home_bp.route('/', methods=['GET'])
 def home():
@@ -16,7 +19,39 @@ def login():
     return render_template("login.html")
 
 @register_bp.route('/register', methods=['GET', 'POST'])
-def register():
+def register() -> Union[str, Dict[str, Union[bool, str]]]:
+    """
+    Render the registration page for `GET` requests and handle user registration for POST requests.
+
+    Returns:
+        - For `GET` requests, returns the rendered `HTML` template for the registration page.
+        - For `POST` requests:
+            - If registration is successful, returns a `JSON` response indicating success.
+            - If an error occurs during registration, returns a `JSON` response indicating failure.
+
+    Raises:
+        Exception: If an unexpected error occurs during user registration.
+
+    Example:
+          To render the registration page:
+            ```python
+            result = register()  # GET request
+            ```
+
+          To handle user registration:
+            ```python
+            data = {
+                'email': 'user@example.com',
+                'password1': 'password123',
+                'isTrainer': False
+            }
+            result = register(request_data=data)  # POST request
+            ```
+
+    Note:
+        - This function expects `JSON` data for `POST` requests. The `JSON` should contain 'email', 'password1', and 'isTrainer' keys.
+        - The 'isTrainer' key indicates whether the user is a Trainer. If present and `True`, the `user_type` is set to 'Trainer'; otherwise, it's set to 'Student'.
+    """
     if request.method == 'POST':
         try:
             data = request.json  # Use request.json to handle JSON data
@@ -33,4 +68,4 @@ def register():
         except Exception as e:
             return jsonify({'success': False, 'message': str(e)})
 
-    return render_template("register.html") 
+    return render_template("register.html")
