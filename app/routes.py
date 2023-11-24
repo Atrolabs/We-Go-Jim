dbe33aba474cd92aba9dc0403dcdceb03b819f5c
Atrolide <loszewskigrzegorz@gmail.com>
2023-11-24 23:16:29 +0100

    Redirection resolved, sub value read from session

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,39 @@
 from flask import Blueprint, render_template, request, jsonify, session
 from services.cognito_service import CognitoService 
-from typing import Union, Dict
+from typing import Union, Tuple
 from utils.cognito_utils import decode_cognito_jwt
 
 # Define Flask Blueprints for different parts of the application
 home_bp = Blueprint("home", __name__)
 login_bp = Blueprint("login", __name__)
 register_bp = Blueprint("register", __name__)
+dashboard_bp = Blueprint("dashboard", __name__)
 
 # Create an instance of CognitoService to interact with Amazon Cognito
 cognito_service = CognitoService()  
 
 @home_bp.route('/', methods=['GET'])
 def home():
+    """Render the default page"""
     return render_template("index.html")
 
+@dashboard_bp.route('/dashboard', methods=['GET'])
+def dashboard():
+    """Render the dashboard page"""
+    # Retrieve user_sub from the session
+    user_sub = session.get('user_sub') 
+    return render_template('dashboard.html', user_sub=user_sub)
+
 @login_bp.route('/login', methods=['GET', 'POST'])
-def login() -> Union[str, Dict[str, Union[bool, str]]]:
+def login() -> Union[str, Tuple[str, int]]:
     """
     Render the login page for `GET` requests and handle user login for POST requests.
 
     Returns:
         - For `GET` requests, returns the rendered `HTML` template for the login page.
         - For `POST` requests:
-            - If login is successful, returns a `JSON` response indicating success.
-            - If an error occurs during login, returns a `JSON` response indicating failure.
+            - If login is successful, returns a `JSON` response indicating success with a status code of `200`.
+            - If an error occurs during login, returns a `JSON` response indicating failure with a status code of `400`.
 
     Raises:
         Exception: If an unexpected error occurs during user login.
@@ -62,38 +71,33 @@ def login() -> Union[str, Dict[str, Union[bool, str]]]:
                 decoded_token = decode_cognito_jwt(access_token)
                 user_sub = decoded_token.get('sub')
 
-                # Note: For debugging purposes only. Remove the following line in production.
-                print(user_sub)
-            
                 # Set up user session
                 session['user_sub'] = user_sub
 
-                # Uncomment the next line to make the session permanent
-               # session.permanent = True
-
                 # Return a success message
-                return jsonify({'success': True, 'message': 'User logged in successfully'})
+                return jsonify({'success': True, 'message': 'User logged in successfully'}), 200
             
             # Handle login failure with a specific error message
-            return jsonify({'success': False, 'message': 'Login failed. Please check your credentials.'})
+            return jsonify({'success': False, 'message': 'Login failed. Please check your credentials.'}), 400
         
         # Handle other exceptions
         except Exception as e:
-            return jsonify({'success': False, 'message': str(e)})
+            return jsonify({'success': False, 'message': str(e)}), 400
 
     return render_template("login.html")
 
 
+
 @register_bp.route('/register', methods=['GET', 'POST'])
-def register() -> Union[str, Dict[str, Union[bool, str]]]:
+def register() -> Union[str, Tuple[str, int]]:
     """
     Render the registration page for `GET` requests and handle user registration for POST requests.
 
     Returns:
         - For `GET` requests, returns the rendered `HTML` template for the registration page.
         - For `POST` requests:
-            - If registration is successful, returns a `JSON` response indicating success.
-            - If an error occurs during registration, returns a `JSON` response indicating failure.
+            - If registration is successful, returns a `JSON` response indicating success with a status code of `200`.
+            - If an error occurs during registration, returns a `JSON` response indicating failure with a status code of `400`.
 
     Raises:
         Exception: If an unexpected error occurs during user registration.
@@ -126,13 +130,12 @@ def register() -> Union[str, Dict[str, Union[bool, str]]]:
             user_type = 'Trainer' if data.get('isTrainer') else 'Student'
 
             response = cognito_service.register_user(email, password, user_type)
-
-            # Note: For debugging purposes only. Remove the following line in production.
-            print(response)
             
-            # Handle register response
-            return jsonify({'success': True, 'message': 'User registered successfully'})
+            # Handle register response with a status code of 200
+            return jsonify({'success': True, 'message': 'User registered successfully'}), 200
         except Exception as e:
-            return jsonify({'success': False, 'message': str(e)})
+            # Handle registration failure with a specific error message and a status code of 400
+            return jsonify({'success': False, 'message': str(e)}), 400
 
     return render_template("register.html")
+
