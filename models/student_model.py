d75a45b034e522a49a2c52a2ac670961610f37d1
juhum <anddd144@gmail.com>
2023-12-07 17:42:22 +0100

    update student_model.py

--- models/student_model.py
+++ models/student_model.py
@@ -1,28 +1,56 @@
 from typing import List, Optional
 from pydantic import BaseModel
 from datetime import datetime
+import json
 
 class ExerciseSet(BaseModel):
     reps: int
     weight: Optional[float]
 
 class Exercise(BaseModel):
-    sets: List[ExerciseSet]
+    name: str
+    series: List[ExerciseSet]
 
 class WorkoutDay(BaseModel):
     exercises: List[Exercise]
 
-class UserWorkoutPlan(BaseModel):
-    Monday: Optional[WorkoutDay] = None
-    Tuesday: Optional[WorkoutDay] = None
-    Wednesday: Optional[WorkoutDay] = None
-    Thursday: Optional[WorkoutDay] = None
-    Friday: Optional[WorkoutDay] = None
-    Saturday: Optional[WorkoutDay] = None
-    Sunday: Optional[WorkoutDay] = None
+class StudentWorkoutPlan(BaseModel):
+    Monday: Optional[List[Exercise]] = None
+    Tuesday: Optional[List[Exercise]] = None
+    Wednesday: Optional[List[Exercise]] = None
+    Thursday: Optional[List[Exercise]] = None
+    Friday: Optional[List[Exercise]] = None
+    Saturday: Optional[List[Exercise]] = None
+    Sunday: Optional[List[Exercise]] = None
 
-class UserModel(BaseModel):
+class StudentModel(BaseModel):
     user_id: str
     user_type: str
-    creation_date: datetime = datetime.now()
-    workout_plan: UserWorkoutPlan = UserWorkoutPlan()
+    email: str
+    workout_plan: Optional[StudentWorkoutPlan] = None
+
+
+
+def save_student_model_to_json(student_model: StudentModel, file_path: str):
+    data = student_model.dict()
+    with open(file_path, 'w') as json_file:
+        json.dump(data, json_file, indent=2)
+
+# Example usage:
+student_data = {
+    "user_id": "123",
+    "user_type": "athlete",
+    "email": "athlete@example.com",
+    "workout_plan": {
+        "Monday": [],
+        "Tuesday": [],
+        "Wednesday": [], 
+        "Thursday": [],
+        "Friday": [],
+        "Saturday": [],
+        "Sunday": []
+    }
+}
+
+student_model_instance = StudentModel(**student_data)
+save_student_model_to_json(student_model_instance, 'student_model.json')
